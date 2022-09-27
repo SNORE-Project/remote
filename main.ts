6dfc2bb6d926a3b97c5826d2d4b17e705575da04
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2022-09-27 09:44:00 +0100

    Add status led

--- main.ts
+++ main.ts
@@ -1,12 +1,32 @@
 radio.setGroup(0);
-let active = false
+let active = false;
+let statusLed = true;
 
-input.onButtonPressed(Button.A, function () {
+const setLed = () => {
+    if (active && statusLed) {
+        led.plot(0, 0);
+    } else {
+        led.unplot(0, 0);
+    }
+}
+
+input.onButtonPressed(Button.A, () => {
     active = !active;
+    setLed();
     if (active) {
         snore.initialise();
+        basic.showString("On");
+    } else {
+        basic.showString("Off");
     }
-})
+});
+
+input.onButtonPressed(Button.B, () => {
+    statusLed = !statusLed;
+    led.plot(0, 0);
+    basic.pause(1000);
+    setLed();
+});
 
 radio.onReceivedValue(snore.receiveData);
-loops.everyInterval(200, () => {if (active){snore.storeData}});
+loops.everyInterval(snore.intervalSize, () => {if (active){snore.storeData}});
