e69b05515e67cdefc634c09110676aa255c49745
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2022-11-18 13:26:04 +0000

    Add pulse saving

--- main.ts
+++ main.ts
@@ -2,5 +2,6 @@ radio.setGroup(0);
 snore.initialise();
 
 radio.onReceivedValue(snore.receiveData);
+radio.onReceivedString(snore.recievePulse);
 
 loops.everyInterval(snore.intervalSize, snore.storeData);
